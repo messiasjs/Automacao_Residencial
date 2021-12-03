22d4e3e3aa129fbb97c89a9f1387c3c2c4f509ac
messiasjs <55643422+messiasjs@users.noreply.github.com>
2021-12-03 01:38:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-
-
 <head>
     <title>Automação Residencial</title>
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
