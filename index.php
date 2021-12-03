d4466dae3f8e295d7c3f44195678509a41e82c80
messiasjs <55643422+messiasjs@users.noreply.github.com>
2021-12-03 01:07:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" type="text/css">
     <!--Importa a biblioteca PAHO -->
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.2/mqttws31.min.js" type="text/javascript"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.2/mqttws31.min.js" type="text/javascript"></script>
     <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>-->
-    <script src="mqttws31.js" type="text/javascript"></script><!---->
+    <!--<script src="mqttws31.js" type="text/javascript"></script><!---->
     <script src="mqtt.js" type="text/javascript"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
    <link rel="stylesheet" href="font-awesome.min.css">
